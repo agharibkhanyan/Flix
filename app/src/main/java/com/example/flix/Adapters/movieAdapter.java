547448910b933d85e171d9842293f8b6240e11cd
Artur Gharibkhanyan <34009630+agharibkhanyan@users.noreply.github.com>
2020-01-25 03:09:34 -0800

    final working

--- app/src/main/java/com/example/flix/Adapters/movieAdapter.java
+++ app/src/main/java/com/example/flix/Adapters/movieAdapter.java
@@ -1,20 +1,28 @@
 package com.example.flix.Adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.Configuration;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityOptionsCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.example.flix.DetailActivity;
 import com.example.flix.Models.Movie;
 import com.example.flix.R;
+
+import org.parceler.Parcels;
 
 import java.util.List;
 
@@ -56,9 +64,12 @@ public class movieAdapter extends RecyclerView.Adapter<movieAdapter.ViewHolder>{
 
     public class ViewHolder extends RecyclerView.ViewHolder {
 
+        RelativeLayout container;
         TextView tvTitle;
         TextView tvOverview;
         ImageView ivPoster;
+        ImageView imageButton;
+        //ImageView ivProfile;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -66,14 +77,22 @@ public class movieAdapter extends RecyclerView.Adapter<movieAdapter.ViewHolder>{
             tvTitle = itemView.findViewById(R.id.tvTitle);
             tvOverview = itemView.findViewById(R.id.tvOverview);
             ivPoster = itemView.findViewById(R.id.ivPoster);
+            imageButton = itemView.findViewById(R.id.imageButton);
+            //ivProfile = itemView.findViewById(R.id.ivProfile);
+            container = itemView.findViewById(R.id.container);
 
 
         }
 
-        public void bind(Movie movie) {
+        public void bind(final Movie movie) {
             tvTitle.setText(movie.getTitle());
             tvOverview.setText(movie.getOverview());
             String imageUrl;
+            imageButton.setEnabled(true);
+            if(movie.getRating()<5)
+                imageButton.setVisibility(View.GONE);
+            else
+                imageButton.setVisibility(View.VISIBLE);
 
             //if in landscape mode
             if(context.getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE){
@@ -83,7 +102,25 @@ public class movieAdapter extends RecyclerView.Adapter<movieAdapter.ViewHolder>{
             else
                 imageUrl = movie.getPostPath();
 
+
+            // 1. Register click listener on the whole row
             Glide.with(context).load(imageUrl).into(ivPoster);
+            container.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // 2. Navigate to a new activity on tap
+                    //passing data from adapter to detail class to display
+                    Intent i = new Intent(context, DetailActivity.class);
+                    //i.putExtra(DetailsActivity.EXTRA_CONTACT,contact);
+                    //ActivityOptionsCompat options = ActivityOptionsCompat.makeSceneTransitionAnimation(context,(View)ivProfile,"profile");
+                    //startActivity(intent,options.toBundle());
+                    i.putExtra("movie", Parcels.wrap(movie));
+
+
+
+                    context.startActivity(i);
+                }
+            });
         }
     }
 }
--- app/src/main/java/com/example/flix/Models/Movie.java
+++ app/src/main/java/com/example/flix/Models/Movie.java
@@ -3,16 +3,23 @@ package com.example.flix.Models;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Parcel
 public class Movie {
 
+    //create movie objects to pass
     String backgroundPath;
     String postPath;
     String title;
     String overview;
+    double rating;
+    int movieId;
+    // empty constructor needed by the Parceler library
+    public Movie(){}
 
     //throw a json exception so the called is responsible of handling the exception
     public Movie (JSONObject jsonObject) throws JSONException {
@@ -20,6 +27,8 @@ public class Movie {
         postPath = jsonObject.getString("poster_path");
         title = jsonObject.getString("title");
         overview = jsonObject.getString("overview");
+        rating = jsonObject.getDouble("vote_average");
+        movieId = jsonObject.getInt("id");
     }
 
     //create json array of movies to send back
@@ -47,5 +56,9 @@ public class Movie {
     public String getOverview() {
         return overview;
     }
+
+    public double getRating(){return rating;}
+
+    public int getMovieId(){return movieId;}
 }
 
